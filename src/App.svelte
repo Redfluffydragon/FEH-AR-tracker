ad53f098007e859e4edcb2a019488239bc17a7be
Redfluffydragon <kderu1420@gmail.com>
2021-09-29 08:50:54 -0600

    Better background, move functions & buttons over to MainTable that only had to do with the table

--- src/App.svelte
+++ src/App.svelte
@@ -4,14 +4,12 @@
 	import Modal from './Modal.svelte';
 
 	import {
-		dataKeys,
-		edit,
 		showColumns,
 		columnData,
-		defaultShow,
 		darkMode,
 		goalColor,
 		thisWeekData,
+		singleOffenseDisplay,
   } from './stores';
 
 	/**
@@ -20,99 +18,65 @@
 	 * input last defense date by number of hours ago
 	 * paste last defense date in?
 	 * settings
-	   * better dark mode
-	   * green total lift if greater than lift goal?
+	   * green total lift if greater than lift goal? Red if it's not possible to make it?
+	   * display number of double vs single offense matches
+		 * scale? for readability
 	 * tooltips or something for mobile to see title text?
 	 * don't store title text and stuff in localStorage? or add version control?
-	 * display number of double vs single offense matches
-	 * Save all past seasons
+	 * Save past seasons
 	   * set starting lift for next season based on total lift from the previous season
+		 * set liftGoal based on last season's lift?
 	 * fix defense margin when it's no longer possible to reach your goal
-	 * change dndzone highlight color
-	 * liftGoal input doesn't display value when page is first loaded
 	 */
 
 	$: localStorage.setItem('showColumns', JSON.stringify($showColumns));
 	$: localStorage.setItem('columnData', JSON.stringify($columnData));
 	$: localStorage.setItem('thisWeekData', JSON.stringify($thisWeekData));
 	$: localStorage.setItem('darkMode', JSON.stringify($darkMode));
+	$: localStorage.setItem('singleOffenseDisplay', JSON.stringify($singleOffenseDisplay));
 
 	onMount(() => {
 		$darkMode && document.body.classList.add('dark-mode');
-	})
+	});
 
-	let tempShowColumns = {};
-	let tempColumnData = [];
-
-	let dragDisabled = true;
-
-	function startEdit() {
-		for (let i of dataKeys) {
-			tempShowColumns[i] = $showColumns[i];
-		}
-		$columnData.forEach((item, idx) => tempColumnData[idx] = item);
-		
-		$edit = true;
-		// Otherwise the drag and drop activates before the hidden columns are shown and it messes everything up
-		setTimeout(() => {
-			dragDisabled = false;
-		}, 20);
-	}
-
-	function saveEdit() {
-		$edit = false;
-		dragDisabled = true;
-	}
-
-	function cancelEdit() {
-		for (let i of dataKeys) {
-			$showColumns[i] = tempShowColumns[i];
-		}
-		tempColumnData.forEach((item, idx) => $columnData[idx] = item);
-
-		$edit = false;
-		dragDisabled = true;
+	const settingsProps = {
+		title: 'Settings',
+		imageBtn: true,
+		src: 'img/settings-icon.png',
 	}
 </script>
+<div class="background"></div>
 
-<img src="{$darkMode ? 'img/FEH_Dokkalfheimr.png' : 'img/BG_Alfheim.png'}" alt="Background art" class="background">
+<Modal props="{settingsProps}">
+	<div slot="content">
+		<button class:active="{!$singleOffenseDisplay}" on:click="{() => {
+			$singleOffenseDisplay = !$singleOffenseDisplay;
+		}}">Display number of double offense matches</button>
+
+		<br>
+
+		<button class:active="{$goalColor}" on:click="{() => {
+			$goalColor = !$goalColor;
+		}}">Color display for goal</button>
+
+		<br>
+
+		<button class:active="{$darkMode}" on:click="{
+			() => {
+				$darkMode ? document.body.classList.remove('dark-mode') : document.body.classList.add('dark-mode');
+				$darkMode = !$darkMode;
+			}
+		}">Dark mode</button>
+		<br>
+		<input type="range" min=1 max=2 step=0.05 value=1 id="zoomSlider">
+		<label for="zoomSlider">Zoom</label>
+	</div>
+</Modal>
 
 <main class="flex flexColumn">
 	<h1>FEH AR season lift calculator</h1>
 
-	<Modal props="{{
-		imageBtn: true,
-		title: 'Settings',
-		src: 'img/settings-icon.png',
-	}}">
-
-		<div slot="content">
-			<button class:active="{$goalColor}" on:click="{() => {
-				$goalColor = !$goalColor;
-			}}">Color display for goal</button>
-			<br>
-			<button class:active="{$darkMode}" on:click="{
-				() => {
-					$darkMode ? document.body.classList.remove('dark-mode') : document.body.classList.add('dark-mode');
-					$darkMode = !$darkMode;
-				}
-			}">Dark mode</button>
-		</div>
-	</Modal>
-
-	<MainTable dragDisabled="{dragDisabled}" />
-
-	<div class="z-0">
-		{#if $edit}
-			<span class="editTip">Drag and drop to rearrange</span>
-			<br>
-			<button on:click="{saveEdit}" class="saveBtn">Save</button>
-			<button on:click="{cancelEdit}">Cancel</button>
-			<button on:click="{() => {$showColumns = defaultShow}}">Show all</button>
-		{:else}
-			<button class="edit-btn" on:click="{startEdit}">Edit</button>
-		{/if}
-	</div>
+	<MainTable />
 
 	<div>
 		<button on:click="{() => localStorage.clear()}">Clear localStorage</button>
@@ -136,7 +100,10 @@
 		--savebtn-bg: rgb(112, 188, 255);
 		--active-highlight: rgb(150, 150, 150);
 		--transparent-bg: rgba(255, 255, 255, 0.8);
+		--bg-img-src: url('img/BG_Alfheim.png');
 		--invert: 0;
+
+		background: var(--bg);
 	}
 
 	:global(body.dark-mode) {
@@ -148,6 +115,7 @@
 		--savebtn-bg: rgb(24, 77, 201);
 		--active-highlight: rgb(0, 0, 0);
 		--transparent-bg: rgba(0, 0, 0, 0.8);
+		--bg-img-src: url('img/FEH_Dokkalfheimr.png');
 		--invert: 100%;
 	}
 
@@ -178,13 +146,6 @@
 		background: var(--savebtn-bg);
 	}
 
-	.background {
-		position: fixed;
-		z-index: -1;
-		overflow: hidden;
-		min-height: 100%;
-	}
-
 	main {
 		text-align: center;
 		padding: 1rem;
@@ -202,15 +163,6 @@
 		text-shadow: 0 0 15px var(--color);
 	}
 
-	.editTip {
-		display: inline-block;
-		padding: 5px 7px;
-		margin-bottom: 1rem;
-		width: auto;
-		background: var(--transparent-bg);
-		border-radius: 3px;
-	}
-
 	.active {
 		box-shadow: inset 0 0 15px 3px var(--active-highlight);
 	}
@@ -221,9 +173,5 @@
 
 	.flex {
 		display: flex;
-	}
-
-	.z-0 {
-		z-index: 0;
 	}
 </style>
